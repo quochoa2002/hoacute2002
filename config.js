38bc98af3f1e024c5a673d64bdf6a68720438699
quochoa2002 <94619905+quochoa2002@users.noreply.github.com>
2021-11-18 20:46:38 +0700

    hoacute

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'yêu HÒA đi, không yêu là phí đấy 😙',
+    desc: 'yêu HÒA đi, không yêu là phí đấy :) ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Tại anh đẹp zai nên em đồng ý phải hông :3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Đồ mê zai  😘😘',
+    messDesc: 'Thích tui rồi thì nhắn tin cho tui đi :3',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://m.me/chillmusic200' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
